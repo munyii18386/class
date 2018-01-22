30baad46ecf8ffb9a8992e7bf6b4c8150800e7c3
mkfreeman <michael.k.freeman@gmail.com>
2018-01-22 10:58:26 -0800

    Add js-intro

---
+++ js-intro/activity-1/js/index.js
@@ -0,0 +1,64 @@
+// Main JavaScript File: enable strict mode
+
+
+/* ******************** Basic variables and methods ******************* */
+
+// Create a string variable, then print it out to the console ("log" it)
+
+
+// Use the length property to see how many characters are in your `str` variable
+// Log the result
+
+
+// Use the `toUpperCase` method to create a new variable `upperStr` that has the `str` value, but all UPPPERCASE.
+
+
+// Use the "split" method create a new variable `words` that is an array of the component words of `str`
+
+
+// Use the length property to see how many words are in your `words` array
+// Log your result
+
+
+// Create an array "breakfast" of everything that you ate for breakfast
+
+
+// Log the phrase 'I had N items for breakfast'
+
+
+// Create an object "meals" with keys for breakfast, lunch, and dinner
+// The values should be arrays of items for each meal
+
+
+// Add "pizza" to your dinner. Log your meals to the console.
+
+
+/* ******************** Basic functions and control structures ******************* */
+
+// Write a function that returns the number of words in a string
+// Hint: use the `split` method
+
+
+// Pass a string to your function to determine that it works
+// Log the result
+
+
+// Write a function to test if a string contains a word, and returns a boolean value.
+
+
+// Pass a phrase and a string to your function to determine that it works.
+// Log the result.
+
+/* ******************** Loops ******************* */
+// Create an array of 40 students by looping through the numbers 1 through 40.
+// Hint: create an empty array to start.
+
+
+// Log your array of students
+
+// Add "(I like programming)" to the end of each student whose number is divisible by 10
+// Remeber that JavaScript starts indexing at *0*
+
+
+// Log your array of students (again)
+
