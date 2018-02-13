62d4d3b019d29540a381c133451f1a3ceae0f9ca
Michael Freeman <michael.k.freeman@gmail.com>
2018-02-12 23:21:46 -0800

    Added instructions

--- react/activity-2/src/App.js
+++ react/activity-2/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
-
+// Import Component
+import { Component } from 'react';
 
 // Create a SearchApp Component
 class SearchApp extends React.Component {
+    // In the constructor, set the initial state of search to empty string
+    // Also *bind `this`* to the handleChange function
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
@@ -11,6 +12,8 @@ class SearchApp extends React.Component {
             search: ''
         };
     }
+
+    // In this event, get the target value, and reset the state of `search`
     handleChange(event) {
         // Get event value
         let searchValue = event.target.value;
@@ -20,16 +23,20 @@ class SearchApp extends React.Component {
             search: searchValue
         });
     }
+
+    // Function to render data
     render() {
         // Filter the table data
         let employees = this.props.data,
             searchString = this.state.search.trim().toLowerCase();
 
+        // If the search string isn't an empty string, filter the results using `.match`
         if (searchString.length > 0) {
             // We are searching. Filter the results.
             employees = employees.filter((e) => e.name.toLowerCase().match(searchString));
         }
-        // Set the `update` property of the `UserInput` element
+
+        // Return a `div` containing a  `UserInput` component and a `Table` component
         return (
             <div>
               <UserInput update={ this.handleChange } />
@@ -44,10 +51,13 @@ class UserInput extends React.Component {
     constructor(props) {
         super(props)
     }
+
+    // Return an input element that, on change, does the event passed to update via props
     render() {
-        return (<div>
-                  <input className="form-control mb-2" placeholder="Search Employees..." onChange={ (e) => this.props.update(e) } />
-                </div>)
+        return (
+            <div>
+              <input className="form-control mb-2" placeholder="Search Employees..." onChange={ (e) => this.props.update(e) } />
+            </div>)
     }
 }
 
@@ -70,10 +80,12 @@ class TableRow extends React.Component {
 }
 ;
 
+// Class for a table
 class Table extends React.Component {
     constructor(props) {
         super(props);
     }
+    // Should return a TableRow component for each element in this.props.data
     render() {
         return (
             <div>
@@ -94,5 +106,5 @@ class Table extends React.Component {
     }
 }
 
-
+// Export the SearchApp
 export default SearchApp;
